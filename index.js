0ea733dea8d1da4f69c9bdce5cfeafaac73579c5
Matthew Dworkin <mdworkin21@gmail.com>
2017-10-19 18:30:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return object = {
+
     object.prop = '1',
     object.prop2 = '2';
+    return object
   }
-}
